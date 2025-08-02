6c8225af0ba1076db1de743d8f594263e4469f07
Ahmedelneel2 <ahmedalaaelneel3@gmail.com>
2025-08-02 18:16:00 +0300

    secount commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import Footer from './assets/Pages/Footer/Footer'
 
     import"@fortawesome/fontawesome-free/css/all.min.css"
 import MainLayout from './assets/Pages/MainLayout/MainLayout'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom'
 import About from './assets/Pages/About/About'
 import Portoflio from './assets/Pages/Poroflio/Portoflio'
 import Contact from './assets/Pages/Contact/Contact'
 import Home from './assets/Pages/Home/Home';
 function App() {
-const router = createBrowserRouter(
+const router = createHashRouter(
  [ {path:"/" , element:<MainLayout/> ,children:[
   {index:true,element:<Home/>},
 {path:'about',element:<About/>},
